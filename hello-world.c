3c1ef691f70c1549d4031284f8e59f6c6342cbfa
KatieWright26 <katie.wrght@gmail.com>
2017-09-15 13:22:34 +1200

    add test files

---
+++ hello-world.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("%c\n", argv[2]);
+    return 0;
+}
+
+
+make program where you type in an argument, iterate across the argument and print out what each character is.
+
+printf("%c\", foo)
